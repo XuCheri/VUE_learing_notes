f26dcfdbdcb8821ecc296925bf6c261f55d8455c
XuCheri <1156429007@qq.com>
2021-06-11 15:05:42 +0800

    完成了ImageLoader组件

--- my-site/my-site/src/components/ImageLoader/index.vue
+++ my-site/my-site/src/components/ImageLoader/index.vue
@@ -1,14 +1,20 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-11 00:47:34
- * @LastEditTime: 2021-06-11 00:55:17
+ * @LastEditTime: 2021-06-11 15:00:55
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \VUE_learing_notes\my-site\my-site\src\components\Imageloader\index.vue
 -->
 <template>
-  <div class="ImagerLoader-container">
-      
+  <div class="ImageLoader-container">
+    <img v-if="!everythingDone" class="placeholder" :src="placeholder" alt="" />
+    <img
+      :src="src"
+      alt=""
+      @load="handleLoad"
+      :style="{ opacity: originOpacity, transition: `${duration}ms` }"
+    />
   </div>
 </template>
 
@@ -28,8 +34,42 @@ export default {
       default: 500,
     },
   },
+  data() {
+    return {
+      originLoaded: false,
+      everythingDone: false,
+    };
+  },
+  computed: {
+    originOpacity() {
+      return this.originLoaded ? 1 : 0;
+    },
+  },
+  methods: {
+    handleLoad() {
+      this.originLoaded = true;
+      setTimeout(() => {
+        this.everythingDone = true;
+        this.$emit("load");
+      }, this.duration);
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped lang="less" >
+@import "~@/styles/mixin.less";
+.ImageLoader-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+  img {
+    .self-fill();
+    object-fit: cover;
+  }
+  .placeholder {
+    filter: blur(2vw);
+  }
+}
 </style>
--- my-site/my-site/src/components/ImageLoader/test.vue
+++ my-site/my-site/src/components/ImageLoader/test.vue
@@ -1,16 +1,18 @@
 <!--
  * @Author: your name
  * @Date: 2021-06-11 00:47:39
- * @LastEditTime: 2021-06-11 00:54:52
+ * @LastEditTime: 2021-06-11 15:05:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \VUE_learing_notes\my-site\my-site\src\components\Imageloader\test.vue
 -->
 <template>
-  <div>
+  <div class="test-container">
       <ImageLoader
       src="https://img1.baidu.com/it/u=3118418006,770648774&fm=26&fmt=auto&gp=0.jpg"
       placeholder="https://img1.baidu.com/it/u=1836487637,1052791322&fm=26&fmt=auto&gp=0.jpg"
+      :duration="3000"
+      @load="handleLoaded"
        />
   </div>
 </template>
@@ -21,8 +23,19 @@ export default {
   components: {
     ImageLoader,
   },
+  methods:{
+    handleLoaded(){
+      console.log("ImageLoader加载完成");
+    },
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.test-container{
+  width: 500px;
+  height: 400px;
+  border:2px solid;
+  margin: 0 auto;
+}
 </style>
